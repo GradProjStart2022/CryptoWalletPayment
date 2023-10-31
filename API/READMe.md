6a39050f23c49adc4fb5f2fe5c4a62c8bc33379b
on99yu <dgjjanggu@naver.com>
2023-10-31 15:00:07 +0900

    clean main branch

--- API/READMe.md
+++
@@ -1,6 +0,0 @@
-npm install
-npm install axios  
-npm install express  
-npm install dotenv  
-npm install ejs  
-npm i socket.io
--- web/managerseller/src/config/FirebaseAPI.js
+++
@@ -1,8 +0,0 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import CautionFirebaseConfig from "./firebaseConfig"
-
-  // Initialize Firebase
-firebase.initializeApp(CautionFirebaseConfig);
-
-export const db = firebase.firestore();
